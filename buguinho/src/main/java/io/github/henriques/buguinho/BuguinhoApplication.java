1716db2ee31df6d904251885982d6be28052a7d1
Henrique A. R. <henrique4r.it@gmail.com>
2025-11-26 18:56:03 -0300

    chore: inicializa o projeto SpringBoot via Maven

---
+++ buguinho/src/main/java/io/github/henriques/buguinho/BuguinhoApplication.java
@@ -0,0 +1,13 @@
+package io.github.henriques.buguinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuguinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuguinhoApplication.class, args);
+	}
+
+}
---
+++ buguinho/src/test/java/io/github/henriques/buguinho/BuguinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.henriques.buguinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuguinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
