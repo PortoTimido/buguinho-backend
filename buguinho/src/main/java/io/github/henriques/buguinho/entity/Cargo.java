74c616aaea3219564c9c456a3a6440edcb4860fc
Carvalh0Henrique <carvalhoghenrique@gmail.com>
2025-11-27 09:16:19 -0300

    refactor(Cargo): Refatora o código se adequando a nova regra de negócio
    Co-authored-by: Henrique A. R. <henrique4r.it@gmail.com>
    Co-authored-by: Carvalh0Henrique <carvalhoghenrique@gmail.com>

--- buguinho/src/main/java/io/github/henriques/buguinho/entity/Cargo.java
+++ buguinho/src/main/java/io/github/henriques/buguinho/entity/Cargo.java
@@ -1,24 +1,8 @@
 package io.github.henriques.buguinho.entity;
 
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.data.mongodb.core.mapping.FieldType;
-import org.springframework.data.mongodb.core.mapping.MongoId;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data //Get, set, equals e hashcode
-@NoArgsConstructor
-@AllArgsConstructor
-@Document(collection = "cargos")
-public class Cargo {
-
-    @MongoId(targetType = FieldType.OBJECT_ID)
-    private String id;
-
-    @Field("nome")
-    private String nome;
-
+public enum Cargo {
+    Desenvolvedor_FrontEnd,
+    Desenvolvedor_BackEnd,
+    Testador,
+    Dba
 }
--- buguinho/src/main/java/io/github/henriques/buguinho/repository/CargoRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.henriques.buguinho.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import io.github.henriques.buguinho.entity.Cargo;
-
-public interface CargoRepository extends MongoRepository<Cargo, String> {
-    
-}
