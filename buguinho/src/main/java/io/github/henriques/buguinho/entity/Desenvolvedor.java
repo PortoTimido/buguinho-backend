c23a29888d9e1c609b312f6625c0b6e147468e1f
Carvalh0Henrique <carvalhoghenrique@gmail.com>
2025-11-27 08:29:21 -0300

    feat(entity): Implementa a classe Cargo ao projeto

---
+++ buguinho/src/main/java/io/github/henriques/buguinho/entity/Desenvolvedor.java
@@ -0,0 +1,24 @@
+package io.github.henriques.buguinho.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "desenvolvedores")
+public class Desenvolvedor {
+    
+    @MongoId(targetType = FieldType.OBJECT_ID)
+    private String id;
+
+    private String nome;
+    private String email;
+    private Cargo cargo;
+
+}
