7ada67f30ee3d27948a5e95f645067b22a073c11
Carvalh0Henrique <carvalhoghenrique@gmail.com>
2025-11-27 08:42:13 -0300

    feat(repository): Implementa a interface com o banco de dados da classe Desenvolvedor

---
+++ buguinho/src/main/java/io/github/henriques/buguinho/repository/DesenvolvedorRepository.java
@@ -0,0 +1,7 @@
+package io.github.henriques.buguinho.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface DesenvolvedorRepository extends MongoRepository<DesenvolvedorRepository, String> {
+    
+}
